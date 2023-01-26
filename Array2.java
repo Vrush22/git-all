1799ef10df5fd6d25b53823c3b2672f38c91f261
vrushali <vrushalihirudkar1@gmail.com>
2023-01-26 12:48:11 +0530

    Array by for loop

---
+++ Array2.java
@@ -0,0 +1,16 @@
+class ArrayDemo2
+{
+	public static void main(String[] args)
+	{
+		int[] a=new int[5];
+		a[0]=21;
+		a[1]=31;
+		a[2]=41;
+		a[3]=51;
+		a[4]=61;
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+	}
+}
